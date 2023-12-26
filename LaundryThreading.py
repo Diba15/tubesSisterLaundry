b4048788a6d37518c7f8a6ddd27e0b31b246af21
billyrefanto <mochbillyrefanto@gmail.com>
2023-12-26 13:53:20 +0700

    Mengubah pesan

--- LaundryThreading.py
+++ LaundryThreading.py
@@ -12,23 +12,30 @@ class Laundry:
     def follow_client(self, client):
         with self.lock:
             self.followers.add(client)
-            print(f"{client} is now following {self.name}")
+            print(f"{client} sekarang anda mengikuti {self.name}")
 
     def send_pickup_info(self, client):
-        pickup_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        date_pickup = time.strftime("%Y-%m-%d")
+        time_pickup = time.strftime("%H:%M:%S")
         with self.lock:
             if client in self.followers:
-                print(f"{self.name} sends pickup info to {client}: Your laundry will be picked up at {pickup_time}")
+                print("==============================================================\n")
+                print(f"{self.name} Mengirim informasi penjempulan ke {client}\n")
+                print(f"laundry {self.name} akan di ambil pada tanggal {date_pickup} pukul {time_pickup}\n")
+                print("==============================================================")
             else:
-                print(f"{client} is not following {self.name}. Please follow to receive updates.")
+                print(f"{client} anda tidak mengikuti {self.name}. Silakan ikuti untuk menerima pembaruan.")
 
     def send_delivery_info(self, client):
-        delivery_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() + 7200))  # 2 hours later
+        date_delivery = time.strftime("%Y-%m-%d")  
+        time_delivery = time.strftime("%H:%M:%S", time.localtime(time.time() + 7200))# 2 hours later
         with self.lock:
             if client in self.followers:
-                print(f"{self.name} sends delivery info to {client}: Your laundry will be delivered at {delivery_time}")
+                print(f"{self.name} Mengirim informasi pengantaran ke {client}\n")
+                print(f"laundry {self.name} akan di antar pada tanggal {date_delivery} pukul {time_delivery}\n")
+                print("==============================================================")
             else:
-                print(f"{client} is not following {self.name}. Please follow to receive updates.")
+                print(f"{client} anda tidak mengikuti {self.name}. Silakan ikuti untuk menerima pembaruan.")
 
     def add_order(self, order):
         with self.lock:
