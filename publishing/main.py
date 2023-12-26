4e9f12c3b806a28b176d8902f5b39a5226b438ac
billyrefanto <mochbillyrefanto@gmail.com>
2023-12-26 16:54:28 +0700

    Resolved kalkulasi harga cuci kering

--- publishing/main.py
+++ publishing/main.py
@@ -26,21 +26,29 @@ Pilih jenis laundry:
 2. Cuci Kering
 ===================================
 """)
-    return input('Masukkan pilihan: ')
+    return input('Masukkan pilihan         : ')
 
 
 def get_processing_time(service_type):
-    print(f"===========[Waktu Pengerjaan {service_type}]==========")
-    print("Pilih waktu pengerjaan:")
-    print("1. 1 Hari (Rp 9.000/kg)")
-    print("2. 2 Hari (Rp 8.000/kg)")
-    print("3. 3 Hari (Rp 7.000/kg)")
-    print("=======================================")
-    return input('Pilih waktu pengerjaan:          ')
+    if service_type == '1':  # Jika cuci basah
+        print(f"============[Waktu Pengerjaan Cuci Basah]===========")
+        print("Pilih waktu pengerjaan:")
+        print("1. 1 Hari (Rp 9.000/kg)")
+        print("2. 2 Hari (Rp 8.000/kg)")
+        print("3. 3 Hari (Rp 7.000/kg)")
+        print("=====================================================")
+    elif service_type == '2':  # Jika cuci kering
+        print(f"===========[Waktu Pengerjaan Cuci Kering]===========")
+        print("Pilih waktu pengerjaan:")
+        print("1. 1 Hari (Rp 8.000/kg)")
+        print("2. 2 Hari (Rp 7.000/kg)")
+        print("3. 3 Hari (Rp 6.000/kg)")
+        print("=====================================================")
+    return input('Pilih waktu pengerjaan   : ')
 
 
 def get_customer_details():
-    name = input("Masukkan nama Anda:        ")
+    name = input("Masukkan nama Anda       : ")
     weight = float(input("Masukkan total berat (kg): "))
     return name, weight
 
@@ -64,33 +72,42 @@ elif laundry_choice == '2':
 client.loop_stop()
 
 service_type = get_service_type()
-processing_time_choice = get_processing_time("cuci basah" if service_type == '1' else "cuci kering")
+processing_time_choice = get_processing_time(service_type)
 
 # Mempersiapkan data pelanggan
 customer_name, total_weight = get_customer_details()
 
 # Logika untuk perhitungan harga dan waktu
 price_per_kg = 0
-if processing_time_choice == '1':
-    price_per_kg = 9000 if service_type == '1' else 8000
-elif processing_time_choice == '2':
-    price_per_kg = 8000 if service_type == '1' else 7000
-elif processing_time_choice == '3':
-    price_per_kg = 7000 if service_type == '1' else 6000
+if service_type == '1':  # Jika cuci basah
+    if processing_time_choice == '1':
+        price_per_kg = 9000
+    elif processing_time_choice == '2':
+        price_per_kg = 8000
+    elif processing_time_choice == '3':
+        price_per_kg = 7000
+elif service_type == '2':  # Jika cuci kering
+    if processing_time_choice == '1':
+        price_per_kg = 8000
+    elif processing_time_choice == '2':
+        price_per_kg = 7000
+    elif processing_time_choice == '3':
+        price_per_kg = 6000
 
 total_price = price_per_kg * total_weight
 
 # Dapatkan waktu saat ini
 current_time = datetime.datetime.now()
 
-client.publish("waktu_penjemputan", current_time.strftime("%Y-%m-%d %H:%M:%S"))
 print("===========================================================================")
+print(f"Detail Pesanan\nNama Pelanggan    : {customer_name}\nJenis Laundry     : {'Cuci Basah' if service_type == '1' else 'Cuci Kering'}"
+      f"\nTotal Berat       : {total_weight} kg\nTotal Harga       : Rp {total_price}")
+print("===========================================================================")
+
+client.publish("waktu_penjemputan", current_time.strftime("%Y-%m-%d %H:%M:%S"))
 print(f"Pengambilan Baju di Laundry {'Bojong' if laundry_choice == '1' else 'Soang'} pada waktu:", current_time.strftime("%Y-%m-%d %H:%M:%S"))
 
 delivery_time = current_time + datetime.timedelta(days=int(processing_time_choice))
 client.publish("waktu_pengantaran", delivery_time.strftime("%Y-%m-%d %H:%M:%S"))
 print(f"Pengantaran Baju di Laundry {'Bojong' if laundry_choice == '1' else 'Soang'} pada waktu:", delivery_time.strftime("%Y-%m-%d %H:%M:%S"))
-print("===========================================================================")
-print(f"\nDetail Pesanan\nNama Pelanggan    : {customer_name}\nJenis Laundry     : {'Cuci Basah' if service_type == '1' else 'Cuci Kering'}"
-      f"\nTotal Berat      : {total_weight} kg\nTotal Harga       : Rp {total_price}")
 print("===========================================================================")
