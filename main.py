737b947968e0fa32e2a2b73dd2b19b2d65f9365c
billyrefanto <mochbillyrefanto@gmail.com>
2023-12-26 13:24:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from LaundryThreading import Laundry
+import time
 
 def menu():
-    print('Pilih Laundry yang mau digunakan: ')
-    print('1. Laundry Bojong')
-    print('2. Laundry Soang')
-    print('3. Exit')
+    laundry_bojong = Laundry("Laundry Bojong")
+    laundry_soang = Laundry("Laundry Soang")
+    
+
+    print("""
+        ====================[M E N U]====================
+        Pilih Laundry yang mau digunakan
+        1. Laundry Bojong
+        2. Laundry Soang
+        3. Exit
+        =================================================
+        """)
     inputUser = input('Masukkan pilihan: ')
+    
     if inputUser == '1':
-        print('Laundry Bojong')
-        print('1. Cuci Kering')
-        print('2. Cuci Basah')
-        print('3. Exit')
+        print("""
+        ================[Laundry Bojong]=================
+        Pilih Laundry yang mau digunakan
+        1. Cuci Kering
+        2. Cuci Basah
+        3. Exit
+        =================================================
+        """)
+        laundry_action = input('Masukkan pilihan: ')
+        if laundry_action == '1':
+            client_name = input('Masukkan nama Anda: ')
+            laundry_bojong.follow_client(client_name)
+            laundry_bojong.send_pickup_info(client_name)
+            time.sleep(3) 
+            laundry_bojong.send_delivery_info(client_name)
+        elif laundry_action == '2':
+            # Tindakan untuk Cuci Basah di Laundry Bojong
+            pass
+        elif laundry_action == '3':
+            print('Exit')
+        else:
+            print('Pilihan tidak ada')
+
     elif inputUser == '2':
-        print('Laundry Soang')
-        print('1. Cuci Kering')
-        print('2. Cuci Basah')
-        print('3. Exit')
+        print(
+        """=================[Laundry Soang]=================
+        Pilih Laundry yang mau digunakan
+        1. Cuci Kering
+        2. Cuci Basah
+        3. Exit
+        =================================================
+        """)
+        laundry_action = input('Masukkan pilihan: ')
+        if laundry_action == '1':
+            client_name = input('Masukkan nama Anda: ')
+            laundry_soang.follow_client(client_name)
+            laundry_soang.send_pickup_info(client_name)
+            time.sleep(3)  # Simulating laundry process time
+            laundry_soang.send_delivery_info(client_name)
+        elif laundry_action == '2':
+            # Tindakan untuk Cuci Basah di Laundry Soang
+            pass
+        elif laundry_action == '3':
+            print('Exit')
+        else:
+            print('Pilihan tidak ada')
+
     elif inputUser == '3':
         print('Exit')
     else:
         print('Pilihan tidak ada')
 
-
 if __name__ == '__main__':
     menu()
-
